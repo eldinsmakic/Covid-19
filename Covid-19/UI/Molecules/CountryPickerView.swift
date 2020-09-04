3020aa91af143a4f29839a7306c76322114d40d6
eldinsmakic <eldinsmakic@hotmail.fr>
2020-09-04 15:30:00 +0200

    [Fix]: correct updating UI with correct property wrapper

--- Covid-19/UI/Molecules/CountryPickerView.swift
+++ Covid-19/UI/Molecules/CountryPickerView.swift
@@ -12,15 +12,15 @@ struct CountryPickerView: View {
 
     @ObservedObject var container: ContainerCovid
     var body: some View {
-        Picker("Country", selection: $container.countryPicker.selectedCountry) {
-            ForEach(0..<container.countryPicker.countrys.count) { index in
-                Text(container.countryPicker.countrys[index]).tag(index)
-                }
-            }.pickerStyle(WheelPickerStyle())
-        .frame(width: nil, height: 40, alignment: .center)
-        .onReceive(container.countryPicker.$selectedCountry) { (index) in
-            container.owidDataManager.getData(fromCountry: container.countryPicker.countrys[index], at: Date())
-        }
+            Picker("Country", selection: $container.countryPicker.selectedCountry) {
+                ForEach(0..<container.countryPicker.countrys.count) { index in
+                    Text(container.countryPicker.countrys[index]).tag(index)
+                    }
+                }.pickerStyle(WheelPickerStyle())
+            .frame(width: nil, height: 40, alignment: .center)
+            .onReceive(container.countryPicker.$selectedCountry) { (index) in
+                container.owidDataManager.getData(fromCountry: container.countryPicker.countrys[index], at: Date())
+            }
     }
 }
 
