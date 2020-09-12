2bf9e4a389f2a57e9ca372b6226668dbb14c8185
eldinsmakic <eldinsmakic@hotmail.fr>
2020-09-12 19:33:39 +0200

    [Fix] (CaseUpdateTest) : Changing test to fit new model

--- Covid-19Tests/CaseUpdateTest.swift
+++ Covid-19Tests/CaseUpdateTest.swift
@@ -11,20 +11,26 @@ import XCTest
 
 class CaseUpdateTest: XCTestCase {
 
-    let infected = 10
-    let death = 20
-    let recovered = 5
+    let totalDeaths: Double = 20
+    let totalInfecteds: Double = 10
+    let totalRecovereds: Double = 5
+    let newDeaths: Double = 0
+    let newInfecteds: Double = 3
+    let newRecovereds: Double = 0
 
     var caseUpdate: CaseUpdate!
 
     override func setUp() {
-        self.caseUpdate = CaseUpdate(infected: self.infected, recovered: self.recovered, death: self.death)
+        self.caseUpdate = CaseUpdate(newDeaths: newDeaths, newInfecteds: newInfecteds, newRecovereds: newRecovereds, totalDeaths: totalDeaths, totalInfecteds: totalInfecteds, totalRecovereds: totalRecovereds)
     }
 
     func testGetter() {
-        XCTAssertEqual(infected, caseUpdate.infected)
-        XCTAssertEqual(death, caseUpdate.death)
-        XCTAssertEqual(recovered, caseUpdate.recovered)
+        XCTAssertEqual(totalDeaths, caseUpdate.totalDeaths)
+        XCTAssertEqual(totalInfecteds, caseUpdate.totalInfecteds)
+        XCTAssertEqual(totalRecovereds, caseUpdate.totalRecovereds)
+        XCTAssertEqual(newDeaths, caseUpdate.newDeaths)
+        XCTAssertEqual(newInfecteds, caseUpdate.newInfecteds)
+        XCTAssertEqual(newRecovereds, caseUpdate.newRecovereds)
     }
 
 }
